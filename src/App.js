99e437a502db73c43039dbb5840af75498476502
sanjeev sharma <sanjeev.sharma@affluencesolutions.com>
2018-09-12 17:00:39 +0530

    Constructor and method

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Constructor from './Constructor.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Constructor />
+      </div>
+    );
+  }
+}
+
+export default App;
